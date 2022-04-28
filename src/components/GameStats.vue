b2e987ec7bb2b9d96c73474145742ff8ce051050
hendrikmitk <hharlichs@googlemail.com>
2022-04-28 12:08:11 +0200

    refactor: extract GameStatsScore component

--- src/components/GameStats.vue
+++ src/components/GameStats.vue
@@ -31,16 +31,7 @@
         </div>
       </div>
       <div class="flex flex-1 flex-col">
-        <div class="flex flex-1 items-start justify-between px-8 pt-5">
-          <span class="font-roboto text-3xl">{{ zeropad(score, 2) }}</span>
-          <span>Score</span>
-        </div>
-        <div
-          class="flex flex-1 items-end justify-between px-8 pb-5 text-gusti-gray-medium"
-        >
-          <span>Round</span>
-          <span class="font-roboto text-3xl">{{ zeropad(round, 2) }}</span>
-        </div>
+        <GameStatsScore :round="round" :score="score" />
       </div>
     </div>
     <div
@@ -52,6 +43,7 @@
 </template>
 
 <script setup>
+import GameStatsScore from '@/components/GameStatsScore.vue';
 import { useColorState } from '@/composables/colorState';
 import { useStatsState } from '@/composables/statsState';
 import zeropad from '@/functions/zeropad';
---
+++ src/components/GameStatsScore.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="flex flex-1 items-start justify-between px-10 pt-5">
+    <span class="font-roboto text-3xl">{{ zeropad(score, 2) }}</span>
+    <span class="text-sm">Score</span>
+  </div>
+  <div
+    class="flex flex-1 items-end justify-between px-10 pb-5 text-gusti-gray-medium"
+  >
+    <span class="text-sm">Round</span>
+    <span class="font-roboto text-3xl">{{ zeropad(round, 2) }}</span>
+  </div>
+</template>
+
+<script setup>
+import zeropad from '@/functions/zeropad';
+
+defineProps({
+  score: {
+    type: Number,
+    required: true,
+  },
+  round: {
+    type: Number,
+    required: true,
+  },
+});
+</script>
