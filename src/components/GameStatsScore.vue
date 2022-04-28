7e5fca1bab6c99ce2574ad81f89ee24705fbd197
hendrikmitk <hharlichs@googlemail.com>
2022-04-28 12:55:34 +0200

    fix: align score elements better

--- src/components/GameStatsScore.vue
+++ src/components/GameStatsScore.vue
@@ -1,13 +1,17 @@
 <template>
   <div class="flex flex-1 items-start justify-between space-x-2 px-10 pt-5">
-    <span class="font-roboto text-3xl">{{ zeropad(score, 2) }}</span>
+    <span class="font-roboto text-3xl leading-none">{{
+      zeropad(score, 2)
+    }}</span>
     <span class="text-sm">Score</span>
   </div>
   <div
     class="flex flex-1 items-end justify-between space-x-2 px-10 pb-5 text-gusti-gray-medium"
   >
     <span class="text-sm">Round</span>
-    <span class="font-roboto text-3xl">{{ zeropad(round, 2) }}</span>
+    <span class="font-roboto text-3xl leading-none">{{
+      zeropad(round, 2)
+    }}</span>
   </div>
 </template>
 
