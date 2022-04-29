c97d08dcc4e2a69c99353927f81ca20a445cd0a3
hendrikmitk <hharlichs@googlemail.com>
2022-04-29 16:28:37 +0200

    refactor: replace integer IDs with UUIDs

--- src/data/colors.js
+++ src/data/colors.js
@@ -1,54 +1,56 @@
+import { v4 as uuidv4 } from 'uuid';
+
 export default [
   {
     color: 'blue',
     shades: [
       {
-        id: 1,
+        id: uuidv4(),
         name: 'Turquoise',
         hex: '#40E0D0',
       },
       {
-        id: 2,
+        id: uuidv4(),
         name: 'Powder Blue',
         hex: '#B0E0E6',
       },
       {
-        id: 3,
+        id: uuidv4(),
         name: 'Sky blue',
         hex: '#ADD8E6',
       },
       {
-        id: 4,
+        id: uuidv4(),
         name: 'Electric blue',
         hex: '#7DF9FF',
       },
       {
-        id: 5,
+        id: uuidv4(),
         name: 'Baby blue',
         hex: '#89CFF0',
       },
       {
-        id: 6,
+        id: uuidv4(),
         name: 'Tiffany blue',
         hex: '#81D8D0',
       },
       {
-        id: 7,
+        id: uuidv4(),
         name: 'Steel Blue',
         hex: '#4682B4',
       },
       {
-        id: 8,
+        id: uuidv4(),
         name: 'Carolina blue',
         hex: '#4B9CD3',
       },
       {
-        id: 9,
+        id: uuidv4(),
         name: 'Turkish blue',
         hex: '#4F97A3',
       },
       {
-        id: 10,
+        id: uuidv4(),
         name: 'Maya blue',
         hex: '#73C2FB',
       },
@@ -58,52 +60,52 @@ export default [
     color: 'red',
     shades: [
       {
-        id: 11,
+        id: uuidv4(),
         name: 'Imperial Red',
         hex: '#ED2939',
       },
       {
-        id: 12,
+        id: uuidv4(),
         name: 'Scarlet',
         hex: '#FF2400',
       },
       {
-        id: 13,
+        id: uuidv4(),
         name: 'Indian Red',
         hex: '#CD5C5C',
       },
       {
-        id: 14,
+        id: uuidv4(),
         name: 'Barn Red',
         hex: '#7C0A02',
       },
       {
-        id: 15,
+        id: uuidv4(),
         name: 'Chili Red',
         hex: '#C21807',
       },
       {
-        id: 16,
+        id: uuidv4(),
         name: 'Ruby',
         hex: '#E0115F',
       },
       {
-        id: 17,
+        id: uuidv4(),
         name: 'Maroon',
         hex: '#800000',
       },
       {
-        id: 18,
+        id: uuidv4(),
         name: 'Fire Brick',
         hex: '#B22222',
       },
       {
-        id: 19,
+        id: uuidv4(),
         name: 'Redwood',
         hex: '#A45A52',
       },
       {
-        id: 20,
+        id: uuidv4(),
         name: 'Carmine',
         hex: '#960018',
       },
