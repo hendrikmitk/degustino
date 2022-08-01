eba591a5c7301254a1d5d579c0165e3def459d42
hendrikmitk <hharlichs@googlemail.com>
2022-08-01 21:27:18 +0200

    feat: add basic desktop layout

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
 <template>
   <div
-    class="flex min-h-screen flex-col space-y-4 bg-gusti-orange-light px-6 py-8 font-raleway text-gusti-gray-strong"
+    class="min-h-screen bg-gusti-orange-light px-6 py-8 font-raleway text-gusti-gray-strong md:py-4"
   >
-    <div class="flex flex-col space-y-4">
-      <h1 class="text-center text-4xl font-bold text-gusti-orange-strong">
-        Degustino
-      </h1>
-      <GameStats />
-    </div>
+    <div class="flex flex-col space-y-4 md:mx-auto md:w-[375px]">
+      <div class="flex flex-col space-y-4">
+        <h1 class="text-center text-4xl font-bold text-gusti-orange-strong">
+          Degustino
+        </h1>
+        <GameStats />
+      </div>
 
-    <GameBoard v-if="isGameOn" />
+      <GameBoard v-if="isGameOn" />
+    </div>
   </div>
 </template>
 
